cc1f794584f00e05e44df15227981e341246d2fa
Sajijola1 <sajijola@gmail.com>
2022-12-25 05:53:36 +0000

    Adds logic to Rock, paper, scissors game

--- manual_rps.py
+++ manual_rps.py
@@ -12,3 +12,36 @@ def get_user_choice():
 
     user_choice = input('Kindly choose between "Rock", "Paper" or "Scissors": ')
     return user_choice
+
+# Choose a winner based on rules of Rock-Paper-Scissors.
+# Returns the winner.
+def get_winner(computer_choice=get_computer_choice(), user_choice=get_user_choice()):
+
+    if computer_choice == "Rock" and user_choice == "Paper":
+        print("You won!")
+    
+    elif computer_choice == "Rock" and user_choice == "Scissors":
+        print("You lost")
+    
+    elif computer_choice == "Rock" and user_choice == "Rock":
+        print("It is a tie!")
+    
+    elif computer_choice == "Paper" and user_choice == "Paper":
+        print("It is a tie!")
+    
+    elif computer_choice == "Paper" and user_choice == "Scissors":
+        print("You won!")
+        
+    elif computer_choice == "Paper" and user_choice == "Rock":
+        print("You lost")
+
+    elif computer_choice == "Scissors" and user_choice == "Paper":
+        print("You lost")
+    
+    elif computer_choice == "Scissors" and user_choice == "Scissors":
+        print("It is a tie!")
+        
+    elif computer_choice == "Scissors" and user_choice == "Rock":
+        print("You won!")
+
+
