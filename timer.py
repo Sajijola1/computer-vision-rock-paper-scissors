a4e4f24f8b0a9ff2feefc30885f486df42169d9e
Sajijola1 <sajijola@gmail.com>
2022-12-31 22:33:45 +0000

    Modifies Timer logic

--- timer.py
+++ timer.py
@@ -5,26 +5,30 @@ def countdown_timer(countdown_time):
     Takes the countown duration in seconds as an argument.
     Prints each second of execution to the console and returns the total execution time to the user"""
     
-    # max_time = 3.00  # Specify the maximum time for the counter
+    # Store the start time
     start_time = time.time()
-    count = 0   # Create a counter variable. We don't want this to be in the while loop or it resets back to with each iteration 
+    
+    # Create a counter variable. We don't want this to be in the while loop or it resets back to with each iteration 
+    count = 0
+    
+    # Inform the user of the countdown
+    print("Starting Countdown")
+
     while True:
-        # If the difference between current time and start_time is 1 sec
+        # After 1 sec
         if (time.time() - start_time) == 1.00:
             # Reset the start time
             start_time = time.time()
-            # Add 1 to the count
-            count += 1        
 
             # Check if the count is equal to the maximum time specified        
             # if round(time.time() - start_time, 2) + count == max_time:
             if count == countdown_time:     # quicker condition
-                break 
-            print(str(time.time() - start_time + count) + " seconds")
+                break   # break out of the loop
+            print(f"{countdown_time - count}!")
+            # Add 1 to the count
+            count += 1
     
-    print(f"{round(time.time() - start_time, 2) + count} seconds elapsed")
-    
-    return count    
+    print("Go! ")
 
 countdown_timer(5)
 
