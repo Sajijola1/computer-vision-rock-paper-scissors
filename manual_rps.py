cfec76c1d2c87f6b58cb72da551ce32f5b8bec71
Sajijola1 <sajijola@gmail.com>
2023-01-24 16:51:09 +0000

    Fixed manual_rps.py

--- manual_rps.py
+++ manual_rps.py
@@ -1,41 +1,8 @@
 import random
-import time
-
-def countdown_timer(countdown_time):
-    """ A simple timer that implements a countdown in python without using the time.sleep() funciton.
-    Takes the countown duration in seconds as an argument.
-    Prints each second of execution to the console and returns the total execution time to the user"""
-    
-    # Store the start time
-    start_time = time.time()
-    
-    # Create a counter variable. We don't want this to be in the while loop or it resets back to with each iteration 
-    count = 0
-    
-    # Inform the user of the countdown
-    print("Starting Countdown")
-
-    while True:
-        # After 1 sec
-        if (time.time() - start_time) == 1.00:
-            # Reset the start time
-            start_time = time.time()
-
-            # Check if the count is equal to the maximum time specified        
-            # if round(time.time() - start_time, 2) + count == max_time:
-            if count == countdown_time:     # quicker condition
-                break   # break out of the loop
-            print(countdown_time - count)
-            # Add 1 to the count
-            count += 1
-    
-    print("Go! ")
 
 # Randomly picks an option from "Rock", "Paper" or "Scissors" for the computer
-def get_computer_choice():
-    
-    # Return the computer's choice
-    return random.choice(["Rock", "Paper", "Scissors"])
+get_computer_choice = lambda : random.choice(["Rock", "Paper", "Scissors"])
+
 
 # Ask the user for an input and return it
 def get_user_choice():
@@ -83,7 +50,6 @@ def get_winner(computer_choice, user_choice):
 
 # Running the game
 def play():
-    countdown_timer(3)
     user_choice = get_user_choice()
     computer_choice = get_computer_choice()
 
